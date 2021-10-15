69564716465bf0b8f9cc2f5255c92c5a73a6dbde
David Jensen <tsourdox@icloud.com>
2021-10-15 11:17:22 +0200

    Present info on home screen

--- screens/BalanceScreen.tsx
+++ screens/BalanceScreen.tsx
@@ -6,10 +6,14 @@ import { useAppSelector } from "../store/store";
 function BalanceScreen() {
     const balance = useAppSelector(selectBalance);
     const transactions = useAppSelector(selectTransactions);
+    const { name, savingsGoal } = useAppSelector(state => state.profile);
 
     return (
         <View style={styles.root}>
+            <Text>Welcome {name}</Text>
             <Text>Balance: {balance}$</Text>
+            <Text>Left to savings goal: {savingsGoal - balance}$</Text>
+
             <Text>Transactions</Text>
             {transactions.map((transaction, index) => (
                 <Text key={index}>{transaction}</Text>
