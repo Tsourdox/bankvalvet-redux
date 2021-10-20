e116098d84831ef61487ea031db52277e61922b9
David Jensen <tsourdox@icloud.com>
2021-10-20 09:30:55 +0200

    Showcase custom tabbar with types

---
+++ navigation/CutstomTabNavigator.tsx
@@ -0,0 +1,24 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { MaterialTopTabDescriptor, MaterialTopTabNavigationProp } from "@react-navigation/material-top-tabs/lib/typescript/src/types";
+import { NavigationState } from "@react-navigation/routers";
+
+interface Props {
+    state: NavigationState;
+    descriptors: MaterialTopTabDescriptor;
+    navigation: MaterialTopTabNavigationProp<{}>;
+    position: 'top' | 'bottom';
+}
+
+const MyTabBar = (props: Props) => {
+    console.log(props);
+    return null;
+}
+
+const Tab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator tabBar={(props: Props) => <MyTabBar {...props} />} >
+    </Tab.Navigator>
+  );
+}
